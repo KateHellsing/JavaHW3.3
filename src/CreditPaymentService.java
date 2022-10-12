316dfb647dfab676a347d4885f36b74e49b21b46
KateHellsing <ekaterina.hellsing999@gmail.com>
2022-10-12 22:53:38 +0300

    CreditPaymentService

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,6 +1,6 @@
 public class CreditPaymentService  {
     public int calculate(int loan, int years, float interest) {
-        // monthly interest value i
+        // ежемесячная процентная стоимость i
         double i = interest / 100 / 12;
         return (int) (loan * (i + i / (Math.pow(1 + i, 12 * years) - 1)));
     }
